09c63ff848558f3fe7baa02dfefad1ab479cf018
pipparichter <philippa.a.richter@gmail.com>
2023-03-18 18:56:47 -0700

    setup and license stuffs

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Find-A-Bug API',
+    version='0.1',    
+    description='A Python API for interacting with the Find-A-Bug database.',
+    url='https://github.com/pipparichter/find-a-bug-api',
+    author='Philippa Richter',
+    author_email='prichter@caltech.edu',
+    install_requires=['requests'])
+
