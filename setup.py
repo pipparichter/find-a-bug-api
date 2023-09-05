9089118c47b7c0c6d8ac7f4764763d0558d4baf7
Philippa Richter <pips.richter@gmail.com>
2023-09-05 11:13:55 -0700

    fixing pip install

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
-
+print(find_packages())
 setup(
     name='Find-A-Bug API',
-    version='0.1',    
+    # version='0.1',    
     description='A Python API for interacting with the Find-A-Bug database.',
     url='https://github.com/pipparichter/find-a-bug-api',
     author='Philippa Richter',
     author_email='prichter@caltech.edu',
-    install_requires=find_packages(include=['exampleproject', 'exampleproject.*']))
+    packages=['fabapi'],
+    install_requires=find_packages(exclude=['fabapi']))
 
